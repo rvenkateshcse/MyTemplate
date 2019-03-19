4baa36cb6e81f2008470f843cbe4027048da0555
Venkatesh <rvenkateshcse@outlook.com>
2019-03-19 19:22:20 +0530

    PDF Creation

---
+++ Templates/PDF.cs
@@ -0,0 +1,13 @@
+﻿namespace Templates
+{
+    internal class PDF
+    {
+        private void CreatePDF()
+        {
+            var Renderer = new IronPdf.HtmlToPdf();
+            var PDF = Renderer.RenderHtmlAsPdf("<h1>Hello IronPdf</h1>");
+            var OutputPath = "HtmlToPDF.pdf";
+            PDF.SaveAs(OutputPath);
+        }
+    }
+}
